13087f798e0e0fd2be5a953e0292b4687a88e0da
Rafael Leonardo Lopes <rafael.lopes24@fatec.sp.gov.br>
2022-03-14 03:25:39 -0300

    criando uma tabela com sequelize, mas se

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const router = express.Router()
+
+const { Sequelize, Op, Model, DataTypes } = require("@sequelize/core")
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "path/to/teste.db"
+})
+
+app.set('view engine', 'ejs');
+
+const User = sequelize.define("User", {
+    id: {type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    idade: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING
+    }
+})
+
+User.sync({ alter: true })
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+router.get('/', function(req, res){
+    var name = req.body.name
+    var idade = req.body.idade
+    var email = req.body.email
+
+    var newUser = User.build({ name: name, idade: idade, email: email})   
+    newUser.save()
+    res.sendFile(path.join(__dirname+'/index.html'))
+})
+
+
+
+app.use('/', router)
+console.log("servidor rodando")
+app.listen(process.env.port || 3000)
+console.log("servidor rodando")
